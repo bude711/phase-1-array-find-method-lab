4945d2e2814443382ecd99d1fdc02135e933df5a
+ <brandi.ude@gmail.com>
2021-10-17 18:08:22 -0400

    commit tree 01eaaab989bcf86f72cada645d35f470699988ac

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function superbowlWin(record){
+  if (record.result === "W") {
+      return record.year; 
+  }
+}
+record.find(superbowlWin);  
+
+
+
+// function superbowlWin(win){
+//     return win.result === 'W';
+// }
+
+// console.log(record.find(superbowlWin));
+
+
+
 
